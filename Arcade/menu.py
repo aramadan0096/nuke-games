3e56be4e06d7098b74fa9c6e60457ebfcb65f4bf
Ahmed Ramadan <>
2025-03-22 11:03:06 +0200

    add credits button

--- Arcade/menu.py
+++ Arcade/menu.py
@@ -1,9 +1,13 @@
 import nuke
+import webbrowser
 
 import blocks
 import monster
 import NukeTower
 
+def open_about_page():
+    webbrowser.open('https://www.linkedin.com/in/a-ramadan0096')
+    
 # Create a custom menu named "AR"
 menu = nuke.menu('Nuke')
 ar_menu = menu.addMenu('AR')
@@ -12,3 +16,9 @@ ar_menu = menu.addMenu('AR')
 ar_menu.addCommand('Games/Arkanoid', 'blocks.start_nuke_game()', 'Ctrl+Alt+B')
 ar_menu.addCommand('Games/Monster', 'monster.start_nuke_game()', 'Ctrl+Alt+M')
 ar_menu.addCommand('Games/Nuke Tower', 'NukeTower.start_icy_tower_game()', 'Ctrl+Alt+T')
+
+# Add a separator
+ar_menu.addSeparator()
+
+# Add an "About" menu item
+ar_menu.addCommand('Credits', 'open_about_page()')
